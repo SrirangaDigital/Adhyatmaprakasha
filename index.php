6aea59bdfbab60dbf114f24d0434bc23a3b3e8a4
shivasdst <shivably04sdst@gmail.com>
2018-05-23 13:35:43 +0530

    Added srirang details in footer

--- index.php
+++ index.php
@@ -58,23 +58,7 @@ session_start();
 			include("include_footer.php")
 		?>
 	</div>
-	<div class="footer">
-		<div class="foot_box">
-			<div class="fleft">
-				&copy;2007-2011 Adhyatmaprakasha Karyalaya, Holenarsipura. All Rights Reserved
-			</div>
-			<div class="fright">
-				<ul>
-					<li><a href="#">Terms of Use</a></li>
-					<li>|</li>
-					<li><a href="#">Privacy Policy</a></li>
-					<li>|</li>
-					<li><a href="php/contact.php">Contact Us</a></li>
-					<li>&nbsp;</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+    <?php include("/php/include_footer_out.php");?>
 </div>
 
 </body>
