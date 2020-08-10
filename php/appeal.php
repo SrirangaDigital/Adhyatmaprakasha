80f3215c4f66197a20bb3046dff08bd41bc905df
Shivashankar <shivably04sdst@gmail.com>
2020-08-10 13:21:28 +0530

    php/appeal.php

--- php/appeal.php
+++ php/appeal.php
@@ -51,7 +51,26 @@
 			<div class="list">
 				<p>ಪ್ರತಿ ಶಾಖೆಯಲ್ಲೂ ಕಾರ್ಯಕಾರಿ ಸಮಿತಿಯು ಅಲ್ಲಿನ ಪಂಡಿತರು ಮುಖ್ಯಸ್ಥರ ಸಹಾಯದಿಂದ ಕಾರ್ಯಚಟುವಟಿಕೆಗಳನ್ನು ವಿಶ್ವಸಮಿತಿಯ ಮೇಲ್ವಿಚಾರಣೆಯಲ್ಲಿ ನಡೆಸುತ್ತದೆ. ಲೆಕ್ಕಪತ್ರಗಳು ಪ್ರತಿ ವರ್ಷವೂ ಆಡಿಟ್ ಆಗುತ್ತದೆ. ಸಂಸ್ಥೆಗೆ 80G ವರ್ಗದ ವರಮಾನ ತೆರಿಗೆಯೂ ಇದೆ. ಆಸಕ್ತರು ನೀಡುವ ಚಂದಾ, ದಾನಗಳಿಂದ ಉತ್ಪನ್ನವಾದ ಬಡ್ಡಿ ಮತ್ತು ಕಾಣಿಕೆಗಳೇ ಕಾರ್ಯಾಲಯದ ಆರ್ಥಿಕ ಜೀವವಾಳ.</p>
 				<p><br /></p>	
-				<p>ಹೊಳೆನರಸೀಪುರದಲ್ಲಿ ಅತಿಥಿ ಗೃಹ, ಅವ್ಯಾಹತವಾಗಿ ನಡೆಯುತ್ತಿರುವ ಗ್ರಂಥಮುದ್ರಣಗಳು ಅಭಿಮಾನಿಗಳ ಔದಾರ್ಯದ ಫಲ. ಎಲ್ಲಾ ಕಾರ್ಯಗಳೂ ಶ್ರೀಶ್ರೀಗಳವರು ಬರೆದಿಟ್ಟಿರುವ, ನಡೆಸಿ ತೋರಿಸಿರುವ ರೀತಿಗೆ ಅನುಸಾರವಾಗಿ ರೂಪಿತವಾಗುತ್ತದೆ. ಇವುಗಳ ಉಸ್ತುವಾರಿ ಹಾಗು ನಡೆಸುವ ಹೊಣೆಯನ್ನು ವಿಶ್ವಸ್ತಸಮಿತಿಯು ಅದರ ಅಧ್ಯಕ್ಷರ ನೇತೃತ್ವದಲ್ಲಿ ಹಾಗು ಹೊಳೆನರಸೀಪುರದಲ್ಲಿ ಮತ್ತು ಬೆಂಗಳೂರಿನಲ್ಲಿ ಕಾರ್ಯಕಾರಿ  ಸಮಿತಿಗಳು ತಮ್ಮ ತಮ್ಮ ಅಧ್ಯಕ್ಷರ ನೇತೃತ್ವದಲ್ಲಿ, ನಿರ್ವಹಿಸುತ್ತಿವೆ.</p>
+				<p>ಹೊಳೆನರಸೀಪುರದಲ್ಲಿ ಅತಿಥಿ ಗೃಹ, ಅವ್ಯಾಹತವಾಗಿ ನಡೆಯುತ್ತಿರುವ ಗ್ರಂಥಮುದ್ರಣಗಳು ಅಭಿಮಾನಿಗಳ ಔದಾರ್ಯದ ಫಲ. ಎಲ್ಲಾ ಕಾರ್ಯಗಳೂ ಶ್ರೀಶ್ರೀಗಳವರು ಬರೆದಿಟ್ಟಿರುವ, ನಡೆಸಿ ತೋರಿಸಿರುವ ರೀತಿಗೆ ಅನುಸಾರವಾಗಿ ರೂಪಿತವಾಗುತ್ತದೆ. ಇವುಗಳ ಉಸ್ತುವಾರಿ ಹಾಗು ನಡೆಸುವ ಹೊಣೆಯನ್ನು ವಿಶ್ವಸ್ತಸಮಿತಿಯು ಅದರ ಅಧ್ಯಕ್ಷರ ನೇತೃತ್ವದಲ್ಲಿ ಹಾಗು ಹೊಳೆನರಸೀಪುರದಲ್ಲಿ ಮತ್ತು ಬೆಂಗಳೂರಿನಲ್ಲಿ ಕಾರ್ಯಕಾರಿ  ಸಮಿತಿಗಳು ತಮ್ಮ ತಮ್ಮ ಅಧ್ಯಕ್ಷರ ನೇತೃತ್ವದಲ್ಲಿ, ನಿರ್ವಹಿಸುತ್ತಿವೆ.<br /><br /><br /></p>
+				<p>The working committee of all branches along with the scholars are running various activities with the trust committee overseeing it. The accounts of the institution is audited every year. All donations to the institution are exempt from tax under 80G. The interest procured through donations by large hearted donors are the financial lifeblood of the institution.</p> 
+				<p><br /></p>
+				<p>The guest house in Holenarsipura, continuous publication of books are all possible only due to the donors and their gracious donations. All activities are carried on as per the written guidance of His Holiness Shri Sachidaanandendra Saraswathi. The responsibility of carrying this forward is taken up by the trust committee and its chairman and along with the working committees with the leadership of the respective chairmen of both Holenarsipura and Bengaluru.</p>
+				<p><br /></p>
+				<p>You can donate to Karyalaya by transferring money to the below accounts and by contacting the Holenarsipura office <b>(Phone No.+91-8175-273820; email: secretary@adhyatmaprakasha.org)</b> or the Bengaluru office <b>(Phone No. +91-80-26765548; email: shankara.bhaskara@gmail.com)</b>, depending on the branch you have transferred.</p>
+				<p><br /></p>				
+				<p><br /></p>
+				<p><b>Bank Details:</b></p>
+				<p><br /></p>
+				<p>NAME: <b>ADHYATMA PRAKASHA KARYALAYA</b></p>
+				<p><br /></p>
+				<p>SBI Holenarasipur; IFSC: <b>SBIN0008040</b>,</p> 
+				<p>SB Ac/NO: <b>10693661944</b></p>
+				<p><br /></p>
+				<p>Karnataka Bank Ltd Holenarasipur; IFSC: <b>KARB0000309</b></p>
+				<p>SB Ac/NO: <b>3092500100000201</b></p>
+				<p><br /></p>
+				<p>SBI - TATA SILK FARM (Bangalore); IFSC: <b>SBIN0040199</b></p>
+				<p>SB A/C No: <b>54010112859</b></p>
 			</div>
 		</div>
 		<?php include("include_footer.php");?>
