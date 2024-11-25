b1efdfc359ed384adcda74932ef73617a86cf9e2
vinaymb312 <skanda312@gmail.com>
2024-11-25 12:19:05 +0530

    ಗೀತಾಜಯಂತೀ ಉತ್ಸವ page added to sidebar

--- include_footer.php
+++ include_footer.php
@@ -25,6 +25,9 @@
 			<span class="news"><a href="php/circulars/intro.php">ಶ್ರೀ ಸಚ್ಚಿದಾನಂದ ಅಧ್ಯಾತ್ಮವಿದ್ಯಾಲಯ - ಪರಿಚಯ ಪತ್ರ ಮತ್ತು ಪಾಠಕ್ರಮ</a></span>
 		</p><br />
 	</div>
+	<p><a href="php/images/apk_page.jpeg" target="_blank"><img style="width: 20%;" src="php/images/apk_page.jpeg" /></a></p>
+	<p>ಗೀತಾಜಯಂತೀ ಉತ್ಸವ</p>
+
 	<p style="text-align: right; font-size: 16px" class="news"><a class="title" href="php/images/Jayanthi.pdf">ವೇದಾಂತ ಸಪ್ತಾಹ ಆಹ್ವಾನ ಪತ್ರಿಕೆ</a></p><br />
     <div class="widget">
 		<div class="title">Keep in touch</div>
