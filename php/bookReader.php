189cce427db437800f24fabb5685b31a09906575
Shivashankar <shivably04sdst@gmail.com>
2025-04-21 15:33:27 +0530

    disabled code for topviewed books

--- php/bookReader.php
+++ php/bookReader.php
@@ -10,22 +10,22 @@
 	$db = @new mysqli('localhost', "$user", "$password", "$database");
 	mysqli_set_charset ( $db , "utf8" );
 	
-	$query = "select * from topviewed where language = '$type' and bookid = $book_id";
-	$result = $db->query($query); 
-	$num_rows = $result ? $result->num_rows : 0;
+	// $query = "select * from topviewed where language = '$type' and bookid = $book_id";
+	// $result = $db->query($query); 
+	// $num_rows = $result ? $result->num_rows : 0;
 	
-	if($num_rows > 0 && !(isset($_SESSION[$type.$book_id]) && $_SESSION[$type.$book_id] != ""))
-	{
-		$row = $result->fetch_assoc();
-		$query = "update topviewed set hits = ".($row["hits"]+1)." , viewed_date =  ".date("Y-m-d")." where bookid = ".$row["bookid"]." and language = '".$type."'";
-		$db->query($query);
-	}
-	elseif($num_rows == 0)
-	{
-		$query = "insert into topviewed values('',$book_id,'$type',1,'".date("Y-m-d")."');";
-		$db->query($query);
-	}
-	$_SESSION[$type.$book_id] = $type.$book_id;
+	// if($num_rows > 0 && !(isset($_SESSION[$type.$book_id]) && $_SESSION[$type.$book_id] != ""))
+	// {
+	// 	$row = $result->fetch_assoc();
+	// 	$query = "update topviewed set hits = ".($row["hits"]+1)." , viewed_date =  ".date("Y-m-d")." where bookid = ".$row["bookid"]." and language = '".$type."'";
+	// 	$db->query($query);
+	// }
+	// elseif($num_rows == 0)
+	// {
+	// 	$query = "insert into topviewed values('',$book_id,'$type',1,'".date("Y-m-d")."');";
+	// 	$db->query($query);
+	// }
+	// $_SESSION[$type.$book_id] = $type.$book_id;
 	//~ *******************Top View Book Hits*************************
 	//~ print_r($_SESSION['sd']);
 	header("Location: bookreader/templates/book.php?".$url);
