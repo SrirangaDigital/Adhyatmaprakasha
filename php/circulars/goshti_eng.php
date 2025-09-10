cc4c52a18bea38ab58c861c8c8a0154478d7e9a5
Shivashankar D <sriranga@Shivashankars-MacBook-Air.local>
2025-09-10 15:50:05 +0530

    removed header n footer in each files

--- php/circulars/goshti_eng.php
+++ php/circulars/goshti_eng.php
@@ -1,48 +1,5 @@
-<?php
-	session_start();
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="shortcut icon" href="images/aplogo.ico">
-<title>Adhyatma Prakash Karyalaya</title>
-<link href="../style/reset.css" media="screen" rel="stylesheet" type="text/css" />
-<link href="../style/style.css" media="screen" rel="stylesheet" type="text/css" />
-</head>
+<?php include(__DIR__ . "/../../inc/include_header.php");?>
 
-<body>
-<div class="page">
-	<div class="header">
-		<div class="logo"><img src="../images/aplogo.png" alt="AP-Logo"/></div>
-		<div class="logokalash"><img src="../images/kalash.png" alt="kalash"/></div>
-		<div class="title">Adhyātmaprakāsha Kāryālaya</div>
-		<div class="subtitle">एतज्ज्ञेयं नित्यमेवात्मसंस्थम् | नातः परं वेदितव्यं हि किञ्चित् ||</div>
-		<div id="nav">
-			<ul>
-				<li><a href="../../index.php">Home</a></li>
-				<li><a href="../about.php">About</a></li>
-				<li><a href="../activity.php">Activities</a></li>
-				<li><a href="../magazine.php">Magazine</a>
-					<ul id="magnav">
-						<li><a href="../volumes.php">Volumes</a></li>
-						<li><a href="../articles.php?letter= ">Articles</a></li>
-						<li><a href="../authors.php?letter= ">Authors</a></li>
-					</ul>
-				</li>
-				<li><a href="../publications.php">Publications</a>
-					<ul id="pubnav">
-						<li><a href="../kannada.php">Kannada Books</a></li>
-						<li><a href="../sanskrit.php">Sanskrit Books</a></li>
-						<li><a href="../english.php">English Books</a></li>
-					</ul>
-				</li>
-				<li><a href="../appeal.php">Appeal</a></li>
-				<li><a class="active" href="../news.php">News</a></li>
-				<li><a href="../contact.php">Contact</a></li>
-			</ul>
-		</div>
-	</div>
 	<div class="content">
 		<div class="col1">
 			<div class="title">Dakshina Bharata Shaankara Vedanta Vidvadgoshti - March 5th to 7th, 2011</div>
@@ -107,21 +64,7 @@
 
 			</p>
 		</div>
-		<div class="col2">
-			<div class="widget">
-				<div class="title">News updates</div>
-				<p>
-					<span class="news"><a href="../php/circulars/intro.php" target="_blank">ಶ್ರೀ ಸಚ್ಚಿದಾನಂದ ಅಧ್ಯಾತ್ಮವಿದ್ಯಾಲಯ - ಪರಿಚಯ ಪತ್ರ ಮತ್ತು ಪಾಠಕ್ರಮ</a></span>
-				</p>
-			</div>
-			<div class="rule"></div>
-			<div class="widget">
-				<div class="title">Top viewed books</div>
-				<p><?php include("../topviewed.php")?></p>
-			</div>
-		</div>
+		<?php include(__DIR__ ."/../../inc/include_sidebar.php");?>
 	</div>
-    <?php include("../include_footer_out.php");?>
-</div>
-</body>
-</html>
+
+<?php include(__DIR__ ."/../../inc/include_footer.php");?>
